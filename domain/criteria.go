d4165001f686d113140e5310b5af05b196363a7a
drjperdior <julio.perdiguer@docplanner.com>
2025-02-14 13:41:52 +0100

    domain error as struct criteria and filter as interface

--- domain/criteria.go
+++ domain/criteria.go
@@ -1,6 +1,6 @@
 package domain
 
-type CriteriaInterface interface {
+type Criteria interface {
 	GetFilters() []*Filter
 	GetSort() string
 	GetSortDir() string
@@ -8,56 +8,9 @@ type CriteriaInterface interface {
 	GetPageSize() int
 }
 
-type Criteria struct {
-	filters  []*Filter
-	sort     string
-	sortDir  string
-	page     int
-	pageSize int
-}
-
-func NewCriteria(filters []*Filter, sort, sortDir string, page, pageSize int) *Criteria {
-	return &Criteria{
-		filters:  filters,
-		sort:     sort,
-		sortDir:  sortDir,
-		page:     page,
-		pageSize: pageSize,
-	}
-}
-
-func (c *Criteria) GetFilters() []*Filter {
-	return c.filters
-}
-
-func (c *Criteria) GetSort() string {
-	return c.sort
-}
-
-func (c *Criteria) GetSortDir() string {
-	return c.sortDir
-}
-
-func (c *Criteria) GetPage() int {
-	return c.page
-}
-
-func (c *Criteria) GetPageSize() int {
-	return c.pageSize
-}
-
-type Filter struct {
-	Name      string
-	Type      string
-	Operation string
-	Value     interface{}
-}
-
-func NewFilter(name, t, operation string, value interface{}) *Filter {
-	return &Filter{
-		Name:      name,
-		Type:      t,
-		Operation: operation,
-		Value:     value,
-	}
+type Filter interface {
+	GetName() string
+	GetType() string
+	GetOperation() string
+	GetValue() interface{}
 }
--- domain/error.go
+++ domain/error.go
@@ -1,6 +1,25 @@
 package domain
 
-type DomainError interface {
-	GetKey() string
-	Error() string
+import (
+	"fmt"
+	"log"
+)
+
+type DomainError struct {
+	Message string
+	Key     string
+}
+
+// Error implements the error interface for DomainError.
+func (e *DomainError) Error() string {
+	return fmt.Sprintf("DomainError{Message: %s, Key: %s}", e.Message, e.Key)
+}
+
+func NewDomainError(message, key string) *DomainError {
+	domainErr := &DomainError{
+		Message: message,
+		Key:     key,
+	}
+	log.Println(domainErr) // Log the error
+	return domainErr
 }
