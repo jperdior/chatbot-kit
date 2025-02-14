7022be2522dd615e6b300c24aa56763618f08d51
drjperdior <julio.perdiguer@docplanner.com>
2025-02-14 15:26:20 +0100

    the best criteria

--- domain/criteria.go
+++ domain/criteria.go
@@ -1,6 +1,6 @@
 package domain
 
-type Criteria interface {
+type CriteriaInterface interface {
 	Filters() []Filter
 	Sort() string
 	SortDir() string
@@ -8,8 +8,54 @@ type Criteria interface {
 	PageSize() int
 }
 
-type Filter interface {
+type Criteria struct {
+	filters  []Filter
+	sort     string
+	sortDir  string
+	page     int
+	pageSize int
+}
+
+func (c *Criteria) Filters() []Filter {
+	return c.filters
+}
+
+func (c *Criteria) Sort() string {
+	return c.sort
+}
+
+func (c *Criteria) SortDir() string {
+	return c.sortDir
+}
+
+func (c *Criteria) Page() int {
+	return c.page
+}
+
+func (c *Criteria) PageSize() int {
+	return c.pageSize
+}
+
+type FilterInterface interface {
 	Name() string
 	Operation() string
 	Value() interface{}
+}
+
+type Filter struct {
+	name      string
+	operation string
+	value     interface{}
+}
+
+func (f *Filter) Name() string {
+	return f.name
+}
+
+func (f *Filter) Operation() string {
+	return f.operation
+}
+
+func (f *Filter) Value() interface{} {
+	return f.value
 }
